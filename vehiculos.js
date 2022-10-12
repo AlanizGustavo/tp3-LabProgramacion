e09c1a8eb269cbe7a18f8a29d2276621a6530318
Gustavo Alaniz <alaniz.gustavo.a@gmail.com>
2022-10-12 18:33:46 -0300

    Grid completado

--- vehiculos.js
+++ vehiculos.js
@@ -1,15 +1,15 @@
-// import vehiculos from './vehiculos.json';
+
 import vehiculos from './vehiculos.json' assert {type: 'json'};
 let grid = document.querySelector('#grid');
 
 vehiculos.forEach(element => {
     const card = document.createElement("div");
     card.classList.add("card");
-    card.innerHTML = `<div>
-                        <p>${element.piloto}</p>
-                        </div>
-                        <div>
-                            <img src="${element.foto}">
-                        </div>`;
+    card.innerHTML = `<div class="contenedorTitulo">
+                        <p class="nombre">${element.piloto}</p>
+                    </div>
+                    <div class="contenedorImagen">
+                        <img class="img" src="${element.foto}">
+                    </div>`;
     grid.appendChild(card);
 });
