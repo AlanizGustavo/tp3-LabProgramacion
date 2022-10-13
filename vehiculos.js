a5b3bdb1e7a33e4a1a870107ac5bc708b2ce1b50
Gustavo Alaniz <alaniz.gustavo.a@gmail.com>
2022-10-13 19:52:41 -0300

    Filtrado de Pilotos terminado

--- vehiculos.js
+++ vehiculos.js
@@ -1,15 +1,58 @@
-
 import vehiculos from './vehiculos.json' assert {type: 'json'};
-let grid = document.querySelector('#grid');
 
-vehiculos.forEach(element => {
+const grid = document.querySelector('#grid');
+const filtro = document.querySelector(".filtros");
+const nombre = document.getElementById("nombre");
+let filtroNombre;
+
+let cargaInicial = function(){
+    vehiculos.forEach( element => {
+        agregarCards(element);
+    }
+)}
+
+let agregarCards = function(element){
     const card = document.createElement("div");
     card.classList.add("card");
     card.innerHTML = `<div class="contenedorTitulo">
-                        <p class="nombre">${element.piloto}</p>
-                    </div>
-                    <div class="contenedorImagen">
-                        <img class="img" src="${element.foto}">
-                    </div>`;
+    <p class="nombre">${element.piloto}</p>
+    </div>
+    <div class="contenedorImagen">
+    <img class="img" src="${element.foto}">
+    </div>`;
     grid.appendChild(card);
-});
+}
+
+filtro.addEventListener("submit", (e) => {
+    e.preventDefault();
+    filtroNombre = nombre.value;
+    vehiculos.forEach(element => {
+        if(element.piloto === filtroNombre){
+            grid.innerHTML="";
+            agregarCards(element);
+        }
+    });
+})
+
+filtro.addEventListener("reset", (e) => {
+    e.preventDefault();
+    grid.innerHTML="";
+    cargaInicial();
+})
+
+cargaInicial();
+
+// vehiculos.forEach(element => {
+//     const card = document.createElement("div");
+//     card.classList.add("card");
+//     card.innerHTML = `<div class="contenedorTitulo">
+//                         <p class="nombre">${element.piloto}</p>
+//                     </div>
+//                     <div class="contenedorImagen">
+//                         <img class="img" src="${element.foto}">
+//                     </div>`;
+//     grid.appendChild(card);
+// });
+
+
+
