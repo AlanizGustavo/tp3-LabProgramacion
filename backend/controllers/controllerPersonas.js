2f7456fd7a0801aaaa143f886d8959868bedcba3
Alaniz Gustavo <alaniz.gustavo.a@gmail.com>
2022-11-24 14:03:48 -0300

    patch persona

--- backend/controllers/controllerPersonas.js
+++ backend/controllers/controllerPersonas.js
@@ -1,5 +1,5 @@
 const modeloPersona = require('../models/personaSchema');
-const { crearPersona, getPersona, deletePersona } = require('../services/servicePersona');
+const { crearPersona, getPersona, deletePersona, editPersona } = require('../services/servicePersona');
 
 const controllerPersona = async (req, res) => {
     const data = req.body;
@@ -15,5 +15,12 @@ const eliminarPersona = async (req,res) => {
     res.send(await deletePersona(id));
 }
 
+const editarPersona = async (req,res) => {
+    const id = req.params;
+    const data = req.body;
+    res.send(await editPersona(id,data));
+}
 
-module.exports = {controllerPersona, obtenerPersonas, eliminarPersona};
+
+
+module.exports = {controllerPersona, obtenerPersonas, eliminarPersona, editarPersona};
--- backend/routes/routePersonas.js
+++ backend/routes/routePersonas.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { controllerPersona, obtenerPersonas, eliminarPersona } = require('../controllers/controllerPersonas');
+const { controllerPersona, obtenerPersonas, eliminarPersona, editarPersona } = require('../controllers/controllerPersonas');
 
 const router = express.Router();
 
+router.patch('/api/personas/editarPersona/:id', editarPersona);
 router.post('/api/personas/crearPersona', controllerPersona);
 router.delete('/api/personas/eliminarPersona/:id', eliminarPersona);
 router.get('/api/personas', obtenerPersonas)
