ebfa2ee6e7b01245fac0dea16f0590bcc2eb4b8a
Leonel Bustamante <aleobustamante@gmail.com>
2022-11-24 14:42:12 -0300

    hotfix

--- client/pages/registro/personas_ABM.js
+++ client/pages/registro/personas_ABM.js
@@ -5,6 +5,8 @@ const closePutPersonas = document.getElementById('closePutPersonas');
 openPutPersonas.addEventListener('click', () => {
     modalputPersonas.classList.add('show');
     cargarTablaPersonas();
+    let searchBox = document.getElementById(("buscarPersona"));
+    funcionSearchBox(searchBox);
 });
 
 closePutPersonas.addEventListener('click', () => {
@@ -14,19 +16,20 @@ closePutPersonas.addEventListener('click', () => {
 async function cargarTablaPersonas() {
     const tablaPersonas = document.getElementById('tablaPutPersonas');
     let personas = await getPersonas();
-    personas.forEach(persona => {
-        let form = document.createElement('form');
-        form.setAttribute('method', 'PATCH');
-        form.setAttribute('action', `http://localhost:9000/api/personas/actualizarPersona/${persona.id}`);
-        form.classList.add('filaABM');
-        form.innerHTML = `
-                <input type="text" value="${persona.nombre}" class="columnaABM">
-                <input type="text" value="${persona.email}" class="columnaABM">
-                <input type="number" value="${persona.edad}" class="columnaABM">
-                <input type="submit" value="✏️" class="columnaABM">
-                <input type="button" value="🗑️" class="columnaABM" onclick="eliminarPersona('${persona.id}')">`
-        tablaPersonas.appendChild(form);
-    });
+    personas.forEach(persona => { tablaPersonas.appendChild(crearFormulario(persona)); });
+}
+function crearFormulario(persona) {
+    let form = document.createElement('form');
+    form.setAttribute('method', 'PATCH');
+    form.setAttribute('action', `http://localhost:9000/api/personas/actualizarPersona/${persona._id}`);
+    form.classList.add('filaABM');
+    form.innerHTML = `
+        <input type="text" value="${persona.nombre}" class="columnaABM">
+        <input type="text" value="${persona.email}" class="columnaABM">
+        <input type="number" value="${persona.edad}" class="columnaABM">
+        <input type="submit" value="✏️" class="columnaABM">
+        <input type="button" value="🗑️" class="columnaABM" onclick="eliminarPersona('${persona._id}')">`
+    return form;
 }
 
 async function getPersonas() {
@@ -37,9 +40,10 @@ async function getPersonas() {
 
 async function eliminarPersona(id) {
     if (confirm('¿Está seguro que desea eliminar esta persona?')) {
-        await fetch(`http://localhost:9000/api/personas/eliminarPersona/${id}`, { method: 'DELETE' });
+        await fetch(`http://localhost:9000/api/personas/eliminarPersona/${id}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' }
+        });
         location.reload();
     }
 }
-
-
