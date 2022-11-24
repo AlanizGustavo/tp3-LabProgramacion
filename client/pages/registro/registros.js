873e31f2620b5569077498e98ad62a8c34df9d64
Leonel Bustamante <aleobustamante@gmail.com>
2022-11-24 00:52:10 -0300

    hot fix

--- client/pages/registro/registros.js
+++ client/pages/registro/registros.js
@@ -1,10 +1,18 @@
+const openPutPersonas = document.getElementById('openPutPersonas');
+const modalputPersonas = document.getElementById('modalPutPersonas');
+const closePutPersonas = document.getElementById('closePutPersonas');
+
+openPutPersonas.addEventListener('click', () => {
+    modalputPersonas.classList.add('show');
+});
+
+closePutPersonas.addEventListener('click', () => {
+    modalputPersonas.classList.remove('show');
+});
+
 const openPersona = document.getElementById('registroPersona');
 const modal_container_persona = document.getElementById('formularioPersona');
 const closePersona = document.getElementById('closePersona');
-
-const openAuto = document.getElementById('registroAuto');
-const modal_container_auto = document.getElementById('formularioAuto');
-const closeAuto = document.getElementById('closeAuto');
 
 openPersona.addEventListener('click', () => {
     modal_container_persona.classList.add('show');
@@ -14,10 +22,12 @@ closePersona.addEventListener('click', () => {
     modal_container_persona.classList.remove('show');
 });
 
-function abrirModalAuto() {
+const openAuto = document.getElementById('registroAuto');
+const modal_container_auto = document.getElementById('formularioAuto');
+const closeAuto = document.getElementById('closeAuto');
 
-}
 openAuto.addEventListener('click', () => {
+    funcionSearchBox();
     modal_container_auto.classList.add('show');
 });
 
@@ -26,13 +36,17 @@ closeAuto.addEventListener('click', () => {
 });
 
 async function editarAuto(id) {
+
     // Cargo valores actuales en el formulario
     const auto = await getAuto(id);
     document.querySelector('#marca').setAttribute('value', auto.marca);
     document.querySelector('#modelo').setAttribute('value', auto.modelo);
     document.querySelector('#anio').setAttribute('value', auto.anio);
-    document.querySelector('#piloto').setAttribute('value', auto.piloto.nombre);
-    document.querySelector('#copiloto').setAttribute('value', auto.copiloto.nombre);
+    console.log(auto);
+    console.log(auto.piloto);
+    console.log(auto.piloto.nombre);
+    document.querySelector('#searchBoxPiloto').setAttribute('value', auto.piloto.nombre);
+    document.querySelector('#searchBoxCopiloto').setAttribute('value', auto.copiloto.nombre);
     document.querySelector('#puntaje').setAttribute('value', auto.puntaje);
     document.querySelector('#foto').setAttribute('value', auto.foto);
 
@@ -60,3 +74,58 @@ async function eliminarAuto(id) {
             headers: { 'Content-Type': 'application/json' }
         });
 }
+
+async function funcionSearchBox() {
+    let searchBox = document.getElementById(("searchBoxPiloto"));
+
+    let listPilotos = document.getElementById("list");
+    let all = await fetch("http://localhost:9000/api/personas/");
+    let pilotosNombres = await all.json();
+    pilotosNombres.forEach(element => {
+        let li = document.createElement("li");
+        li.textContent = element.nombre;
+        li.addEventListener('click', () => {
+            searchBox.value = element.nombre;
+            li.remove();
+            lista.innerHTML = "";
+        });
+        listPilotos.appendChild(li);
+    });
+
+    const filtrar = async () => {
+        filtroNombre = searchBox.value.toLowerCase();
+
+
+        const request = await fetch(`http://localhost:9000/api/vehiculos/${filtroNombre}`);
+        const pilotos = await request.json();
+
+
+        let lista = document.getElementById("list");
+
+        lista.innerHTML = "";
+        if (request.status === 200) {
+            if (pilotos.length > 0) {
+                pilotos.forEach(element => {
+                    let li = document.createElement("li");
+                    li.textContent = element.piloto.nombre;
+                    li.addEventListener('click', () => {
+                        searchBox.value = element.piloto.nombre;
+                        lista.innerHTML = "";
+                    });
+                    lista.appendChild(li);
+                });
+            } else {
+                let li = document.createElement("li");
+                li.textContent = "No se encontraron resultados";
+                lista.appendChild(li);
+            }
+        } else {
+            let li = document.createElement("li");
+            li.textContent = "No se encontraron resultados";
+            lista.appendChild(li);
+        }
+    }
+    searchBox.addEventListener("keyup", filtrar);
+}
+
+
